2a65797eec706c538c5a160e92f1600bfea2ea7f
Michael Vlach <vlach.michael@gmail.com>
2026-07-28 21:47:44 +0200

    impl

--- agplatform/src/test_platform.rs
+++ agplatform/src/test_platform.rs
@@ -1,6 +1,10 @@
-use crate::Platform;
+mod test_env;
 
-/// Gated by the `testing` feature flag.
+use crate::Env;
+use crate::Platform;
+use crate::test_platform::test_env::TestEnv;
+
+/// Enabled by the `testing` feature flag.
 ///
 /// The test platform is a memory-only but fully functional
 /// mock implementation of the `Platform` trait. It is used
@@ -17,6 +21,19 @@ use crate::Platform;
 /// let test_platform = TestPlatform;
 /// do_something(&test_platform);
 /// ```
-pub struct TestPlatform;
+pub struct TestPlatform {
+    pub env: TestEnv,
+}
 
-impl Platform for TestPlatform {}
+impl Platform for TestPlatform {
+    fn env(&self) -> &impl Env {
+        &self.env
+    }
+}
+
+/// Returns an instance of the `TestPlatform` struct
+/// that implements the `Platform` trait via the mock
+/// implementations.
+pub fn test_platform() -> TestPlatform {
+    TestPlatform { env: TestEnv }
+}
--- agplatform/tests/test_platform/mod.rs
+++ agplatform/tests/test_platform/mod.rs
@@ -1,4 +1,8 @@
+use agplatform::Platform;
+use agplatform::test_platform::test_platform;
+
 #[test]
-fn test_platform() {
-    let _ = agplatform::test_platform::TestPlatform;
+fn test_platform_() {
+    let platform = test_platform();
+    let _env = platform.env();
 }
