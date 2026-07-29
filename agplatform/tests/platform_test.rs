d190442e5f064705e3d4a31aca7e4acd5b08aeda
Michael Vlach <vlach.michael@gmail.com>
2026-07-29 21:30:36 +0200

    [agplatform] Add Error  #19 (#20)

--- agplatform/tests/platform_test.rs
+++ agplatform/tests/platform_test.rs
@@ -1,10 +1,11 @@
 use agplatform::Platform;
 
 #[cfg(feature = "testing")]
-mod test_platform;
+mod test_platform_test;
 
 #[test]
-fn platform() {
+fn public_types() {
     let platform = agplatform::platform();
     let _env = platform.env();
+    let _error = agplatform::Error::env("some error");
 }
