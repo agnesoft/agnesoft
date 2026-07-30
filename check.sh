e2f652af2a19cdf0704767d952d398fa097bde47
Michael Vlach <vlach.michael@gmail.com>
2026-07-30 09:51:07 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,14 +1,42 @@
-echo "Linting"
-cargo clippy --all-targets --all-features --fix --allow-dirty
+export CARGO_TERM_COLOR=always
 
-echo "Formatting"
-cargo fmt
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NONE='\033[0m'
 
-echo "Unit tests"
-cargo test --all-features --lib
+function validate() {
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}FAILED${NONE}"
+        echo "$OUT"
+        exit 1
+    else
+        echo -e "${GREEN}OK${NONE}"
+    fi
+}
 
-echo "Doc tests"
-cargo test --all-features --doc
+echo -ne "${GREEN}Linting${NONE} ... "
+OUT=$(cargo clippy --all-targets --all-features --fix --allow-dirty 2>&1)
+validate
 
-echo "Integration tests"
-cargo test --all-features --test '*'
+echo -ne "${GREEN}Formatting${NONE} ... "
+OUT=$(cargo fmt 2>&1)
+validate
+
+echo -ne "${GREEN}Doc tests${NONE} ... "
+OUT=$(cargo test --all-features --doc 2>&1)
+validate
+
+echo -ne "${GREEN}Unit+integration tests${NONE} ... "
+OUT=$(cargo llvm-cov --all-features --show-missing-lines --no-report 2>&1)
+validate
+
+echo -ne "${GREEN}Coverage${NONE} ... "
+OUT=$(cargo llvm-cov report --fail-uncovered-lines 0 --fail-uncovered-regions 0 --fail-uncovered-functions 0 2>&1)
+if [ $? -ne 0 ]; then
+    echo -e "${RED}FAILED${NONE}"
+    echo "$OUT"
+    cargo llvm-cov report --html --open
+    exit 1
+else
+    echo -e "${GREEN}OK${NONE}"
+fi
